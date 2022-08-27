f9a48249e937a9f0137737329200b5bf68171b24
Raymund De La Victoria <RDeLaVictoria@ph.drakeintl.com>
2022-08-27 17:18:01 +0800

    Add project files.

---
+++ DataStore/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Palindrome
+{
+    class PalindromicSubstring
+    {
+        public static void isPalindrome(string str, int left, int right, HashSet<string> set)
+        {
+            while ((left >= 0 && right < str.Length) && (str[left] == str[right]))
+            {
+                set.Add(str.Substring(left, right + 1 - left));
+                left--;
+                right++;
+            }
+        }
+        public static void generateAllSubstring(string str)
+        {
+            var set = new HashSet<string>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                PalindromicSubstring.isPalindrome(str, i, i, set);
+                // odd length palindrome
+                PalindromicSubstring.isPalindrome(str, i, i + 1, set);
+            }
+            Console.WriteLine(string.Join(" ", set));
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a String: ");
+            var input = Console.ReadLine();
+            PalindromicSubstring.generateAllSubstring(input);
+        }
+    }
+}
---
+++ Palindrome/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Palindrome
+{
+    class PalindromSubstring
+    {
+        //Determines if the given string is palindrome by considering that the left and right of every palindrome substring is equal
+        public static void checkPalindrome(string str, int left, int right, HashSet<string> set)
+        {
+            while ((left >= 0 && right < str.Length) && (str[left] == str[right]))
+            {
+                set.Add(str.Substring(left, right + 1 - left));
+                left--;
+                right++;
+            }
+        }
+
+        // Optimized method
+        public static void extractAllSubstrings(string str)
+        {
+            var set = new HashSet<string>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                // Determines the even lenght palindrome substring
+                PalindromSubstring.checkPalindrome(str, i, i, set);
+                // Determines the odd lenght palindrome substring
+                PalindromSubstring.checkPalindrome(str, i, i + 1, set);
+            }
+            Console.WriteLine("\nPalindromes: ");
+            Console.WriteLine(string.Join(", ", set));
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("Extracts All the Palindrome From a Given String");
+            Console.WriteLine("-----------------------------------------------\n");
+            Console.WriteLine("Enter a String: ");
+            var input = Console.ReadLine();
+            PalindromSubstring.extractAllSubstrings(input);
+        }
+    }
+}
